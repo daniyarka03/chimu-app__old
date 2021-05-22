6936d38d2081d87cb39a935164f306258ed8f978
Daniyar <orazov.video@gmail.com>
2021-05-22 13:23:52 +0600

    Commit 1.3 (Beta 1.6.4)
    1. Добавлены новые стили в list_objects.php
    2. Скрипт еще не до конца завершен, возможны баги

--- index.php
+++ index.php
@@ -5,12 +5,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="widadditional_modalth=device-width, initial-scale=1.0">
     <title>Главная страница</title>
+    <link rel="stylesheet" href="css/sidebar.css" />
 </head>
 <body>
+    <?php include './php/components/sidebar.php' ?>
     <?php if ($_COOKIE['user'] == ''): ?>
-    <a href="./register">Регистрация</a>
-    <a href="./login">Авторизация</a>
-
+        <a href="./register">Регистрация</a>
+        <a href="./login">Авторизация</a>
     <?php else: ?>
         <p>Привет <?=$_COOKIE['user']?>. Чтобы выйти нажмите <a href="./php/exit.php">здесь</a></p>
         <a href="./profile">Зайти в профиль</a> <br>
@@ -18,7 +19,6 @@
         <a href="./list_objects">Показать объекты</a>
         <a href="./list_users">Показать пользователей</a>
         <a href="./notifications">Уведомление</a>
-
     <?php endif; ?>
 </body>
 </html>
--- list_objects.php
+++ list_objects.php
@@ -1,103 +1,110 @@
 <?php
-    try {
-        require "php/includes/db.php";
+try {
+    require "php/includes/db.php";
 
-        if(isset($_GET['type'])) {
-            $type = $_GET['type'];
-        } else {
-            $type = 'id';
-        }
-
-        if(isset($_GET['sort'])) {
-            $status = $_GET['sort'];
-        } else {
-            $status = 'DESC';
-        }
-
-
-        $query = $_GET['query'];
-        if (isset($_GET['query'])) {
-            $projects = R::findAll('projects', "title LIKE '%$query%'");
-            if (isset($_GET['type'])) {
-                $projects = R::findAll('projects', ("title LIKE '%$query%' ORDER BY " . $type . ' ' . $status));
-            }
-
-        } else {
-            $projects = R::findAll('projects', ('ORDER BY ' . $type . ' ' . $status));
-        }
-        $id = $_COOKIE['id'];
-
-        $status == 'DESC' ? $status = 'ASC' : $status = 'DESC';
-
-
-        $test = str_split($_SERVER['REQUEST_URI']);
-        $e = array_search('q', $test);
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <style>
-        div {
-            width: 400px;
-            min-height: 80px;
-            height: auto;
-            background-color: #c4c4c4;
-            border: 1px solid #333;
-        }
-    </style>
-</head>
-<body>
-    <a href="add_objects.php"><button>Add object</button></a>
-    <a href="index.php"><button>Main menu</button></a><br /> <br>
-    <?php
-    if ($e) {
-    ?>
-
-    <a href='?query=<?= $_GET['query'] ?>&&type=id&&sort=<?=$status?>'><input type="submit" value="Сортировка по дате"/></a>
-    <a href='?query=<?= $_GET['query'] ?>&&type=title&&sort=<?=$status?>'><button type="submit">Сортировка по алфавиту</button></a>
-    <a href='?query=<?= $_GET['query'] ?>&&type=category&&sort=<?=$status?>'><button type="submit">Сортировка по категориям</button></a>
-
-    <?php
+    if (isset($_GET['type'])) {
+        $type = $_GET['type'];
     } else {
-    ?>
+        $type = 'id';
+    }
 
-    <a href='?type=id&&sort=<?=$status?>'><input type="submit" value="Сортировка по дате"/></a>
-    <a href='?type=title&&sort=<?=$status?>'><button type="submit">Сортировка по алфавиту</button></a>
-    <a href='?type=category&&sort=<?=$status?>'><button type="submit">Сортировка по категориям</button></a>
+    if (isset($_GET['sort'])) {
+        $status = $_GET['sort'];
+    } else {
+        $status = 'DESC';
+    }
 
-    <?php
+
+    $query = $_GET['query'];
+    if (isset($_GET['query'])) {
+        $projects = R::findAll('projects', "title LIKE '%$query%'");
+        if (isset($_GET['type'])) {
+            $projects = R::findAll('projects', ("title LIKE '%$query%' ORDER BY " . $type . ' ' . $status));
         }
-    ?>
+    } else {
+        $projects = R::findAll('projects', ('ORDER BY ' . $type . ' ' . $status));
+    }
+    $id = $_COOKIE['id'];
 
-    <form action="list_objects.php" method="get">
-        <input type="search" name="query" placeholder="Поиск..." />
-        <input type="submit" value="Поиск">
-    </form>
+    $status == 'DESC' ? $status = 'ASC' : $status = 'DESC';
 
-    <?php
-        foreach($projects as $project) {
+
+    $test = str_split($_SERVER['REQUEST_URI']);
+    $e = array_search('q', $test);
+?>
+    <!DOCTYPE html>
+    <html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="css/list_objects.css" />
+        <link rel="stylesheet" href="css/sidebar.css" />
+        <title>Document</title>
+
+    </head>
+
+    <body>
+        <?php include './php/components/sidebar.php' ?>
+        <main>
+
+            <section class="search">
+                <h2 class="search__title">Поиск проектов</h2>
+                <form class="search-form">
+                    <img src="img/search/search.svg" alt="search__icon" class="search-form__img">
+                    <input type="text" class="search-form__input" name="query" placeholder="Введите текст" />
+                    <img src="img/search/filter.svg" alt="search__icon" class="search-form__img icon__filter">
+                </form>
+            </section>
+
+
+            <section class="search-sort">
+                <h2 class="search-sort__title">Сортировать</h2>
+                <div class="search-sort__block">
+                <?php
+                    if ($e) {
+                ?>
+
+                        <a href='?query=<?= $_GET['query'] ?>&&type=id&&sort=<?= $status ?>'><input class="search-sort__btn" type="submit" value="По дате" /></a>
+                        <a href='?query=<?= $_GET['query'] ?>&&type=title&&sort=<?= $status ?>'><button class="search-sort__btn" type="submit">По дате</button></a>
+                        <a href='?query=<?= $_GET['query'] ?>&&type=category&&sort=<?= $status ?>'><button class="search-sort__btn" type="submit">По тегам</button></a>
+
+                    <?php
+                    } else {
+                    ?>
+
+                        <a href='?type=id&&sort=<?= $status ?>'><input class="search-sort__btn" type="submit" value="По дате" /></a>
+                        <a href='?type=title&&sort=<?= $status ?>'><button class="search-sort__btn" type="submit">По алфавиту</button></a>
+                        <a href='?type=category&&sort=<?= $status ?>'><button class="search-sort__btn" type="submit">По тегам</button></a>
+
+                    <?php
+                    }
+                    ?>
+                </div>
+            </section>
+
+
+
+
+            <?php
+            foreach ($projects as $project) {
             ?>
                 <div>
-                    <h4><?= $project['title']?></h4>
-                    <span><?= $project->creator_name?></span> <br>
-                    <span><?= $project->category?></span> <br>
-                    <a href="project.php?id=<?= $project->id?>">View project</a>
-                    <a href="update_objects.php?id=<?= $project->id?>">Update data</a>
-                    <a href="php/delete.php?id=<?= $project->id?>">Delete data</a>
+                    <h4><?= $project['title'] ?></h4>
+                    <span><?= $project->creator_name ?></span> <br>
+                    <span><?= $project->category ?></span> <br>
+                    <a href="project.php?id=<?= $project->id ?>">View project</a>
+                    <a href="update_objects.php?id=<?= $project->id ?>">Update data</a>
+                    <a href="php/delete.php?id=<?= $project->id ?>">Delete data</a>
                 </div>
-                <?php
+        <?php
             }
         } catch (Throwable $e) {
             echo $e;
         }
-    ?>
+        ?>
+        </main>
+    </body>
 
-
-    
-</body>
-</html>
+    </html>
